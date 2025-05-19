67f1ffb9ebe3388b1acf13d9471f0543feef9b20
Dungar93 <147374359+Dungar93@users.noreply.github.com>
2025-05-19 18:40:35 +0000

    name update

---
+++ basics/06_date.js
@@ -0,0 +1,8 @@
+// date 
+
+let myDate =  new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
