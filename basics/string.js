13216cb7b547e1f46ac7bf6ab82e1d1032fe9636
Dungar93 <147374359+Dungar93@users.noreply.github.com>
2025-05-19 17:11:07 +0000

    string basics

---
+++ basics/string.js
@@ -0,0 +1,24 @@
+const name  =  "Dungar soni"
+const repoCount  = 19;
+// console.log(name + repoCount + " soni"); we do not use this nowadays 
+
+console.log(`hey !! how are you doing may name is ${name} and my age is ${repoCount}`);
+const newName  =  new String('DUNGARSONI')
+console.log(newName[0]);
+console.log(newName.length);
+console.log((newName.toLowerCase()));
+console.log(newName.charAt(2));
+console.log(newName.indexOf('N'));
+
+
+const newString  = newName.substring(0,2);
+console.log(newString);
+
+const id = "dunar@123"
+console.log(id.replace('123','soni'))
+
+
+
+
+
+
