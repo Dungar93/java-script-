fd0a19e2e97b307116b83bd922bb45f5c0177bb5
Dungar93 <147374359+Dungar93@users.noreply.github.com>
2025-05-19 18:23:01 +0000

    nums and maths opeations

---
+++ basics/nums&math.js
@@ -0,0 +1,32 @@
+const score  =  400
+console.log(score)
+
+const balance  =  new Number(100)
+// console.log(balance);
+// console.log(balance.toString.length);
+// console.log(balance.toFixed(2));
+
+const othernumber  =  123.56
+// console.log(othernumber.toPrecision(3));
+ const hundred =  1000000;
+//  console.log(hundred.toLocaleString('en-IN'));
+
+
+
+ 
+
+
+
+//**********maths ************* */
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.3))
+
+console.log(Math.max(4,5,6,6));
+ console.log(Math.random());
+ 
+
+
+
